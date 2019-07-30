474e21aa4c4293f5c8f0f1b09d0851789c6224b9
CharlotteMaher <51957601+CharlotteMaher@users.noreply.github.com>
2019-07-30 14:05:22 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 # Data stored on individual pages not accessible from one single page
 # typical URL is https://paulbradshaw.github.io/scraping-for-everyone/scottishschools/iSchoolid_5237521.html
 # Need to cycle through a list of those codes
+#this scraper is for only five of the schools due to time constraints- you can add more ID codes- follow link in book to do this. 
 #If you want to understand this scraper - start at the bottom where it says 'base_url' (line 40 or so)
 import scraperwiki
 import lxml.html
@@ -9,16 +10,16 @@ import lxml.html
 #The 'scrape_page' function also passed the contents of the page to this function as 'root'
 def scrape_table(root):
     #Use cssselect to find the contents of a particular HTML tag, and put it in a new object 'rows'
-    #there's more than one table, so we need to specify the class="destinations", represented by the full stop
+    #there's more than one table, so we need to specify the class="destinations", represented by the full stop- dot shows class #is ID
     rows = root.cssselect("table.destinations tr")
     for row in rows:
         #Create a new empty record
         record = {}
-        #Assign the contents of <td to a new object called 'table_cells'
+        #Assign the contents of <td to a new object (variable) called 'table_cells'
         table_cells = row.cssselect("td")
         #If there's anything
         if table_cells: 
-            #Put the contents of the first <td into a record in the column 'FSM'
+            #Put the contents of the first <td into a record in the column 'FSM' for free school meals
             record['FSM'] = table_cells[0].text
             #this takes the ID number, which has been named item in the for loop below
             record['ID'] = item
@@ -28,10 +29,10 @@ def scrape_table(root):
 
 #this creates a new function and (re)names whatever parameter is passed to it - i.e. 'next_link' below - as 'url'
 def scrape_page(url):
-    #now 'url' is scraped with the scraperwiki library imported above, and the contents put into a new object, 'html'
+    #now 'url' is scraped with the scraperwiki library imported above, and the contents put into a new object (variable), 'html'
     html = scraperwiki.scrape(url)
     print html
-    #now we use the lxml.html function imported above to convert 'html' into a new object, 'root'
+    #now we use the lxml.html function imported above to convert 'html' into a new object (variable), 'root'
     root = lxml.html.fromstring(html)
     #now we call another function on root, which we write - above
     scrape_table(root)
@@ -40,9 +41,11 @@ def scrape_page(url):
 base_url = 'https://paulbradshaw.github.io/scraping-for-everyone/scottishschools/iSchoolid_'
 #And these are the numbers which we need to complete that URL to make each individual URL
 #This list has been compiled using the =JOIN formula in Google Docs on a column of school codes
-schoolIDs =['5237521','5244439','5237629','5237823','5234026']
+schoolIDs =['5237521','5244439','5237629','5237823','5234026'] #these are a list due to [']
 #go through the schoolIDs list above, and for each ID...
 for item in schoolIDs:
+    #item is the name of the variable created when the for loop iterates through the list of school ID codes: 
+    #in other words it is the ID code of the particular page that’s being scraped.
     #show it in the console
     print item
     #create a URL called 'next_link' which adds that ID to the end of the base_url variable
